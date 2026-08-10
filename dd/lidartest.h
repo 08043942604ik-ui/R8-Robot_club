b9a5eb2f7086c091caddc59175d058e37882ddb1
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-08-10 12:57:23 +0900

    i

--- dd/lidartest.h
+++ dd/lidartest.h
@@ -3,16 +3,6 @@
 #include <string>
 
 class Lidarinit {
-public :
-
-Lidarinit() = default;
-
-bool initi();
-bool start();
-void End();
-
-
-
 private :
 CYdLidar Ldr;
 
@@ -33,7 +23,76 @@ float maxum = 180.0f; //何度から何度までの景色を切り取るかと�
 float minum = -180.0f;
 float scanrangemax = 16.f; //何メートルから何メートルまでの壁を検知対象にするかという設定で
 float scanrangemin = 0.1f;
+
+
+
+
+public :
+
+Lidarinit() = default;
+
+bool initi() {
+Ldr.setlidaropt(LidarPropSerialPort, port.c_str(), port.size());
+Ldr.setlidaropt(LidarPropIgnoreArray, Ignore_array.c_str(), Ignore_array.size());
+Ldr.setlidaropt(LidarPropBaudrate, &baudrate, sizeof(int));
+Ldr.setlidaropt(LidarPropLidarType, &lidar_type, sizeof(int));
+Ldr.setlidaropt(LidarPropSampleRate, &sample_rate, sizeof(int));
+Ldr.setlidaropt(LidarPropDeviceType, &device_type, sizeof(int));
+Ldr.setlidaropt(LidarPropAbnormalCheckCount, &abnormal_count, sizeof(int));
+Ldr.setlidaropt(LidarPropFixedResolution, &fixed_resolution, sizeof(bool));
+Ldr.setlidaropt(LidarPropScanFrequency, &scan_frequency, sizeof(float));
+Ldr.setlidaropt(LidarPropAutoReconnect, &auto_reconnect, sizeof(bool));
+Ldr.setlidaropt(LidarPropSingleChannel, &single_channel, sizeof(bool));
+Ldr.setlidaropt(LidarPropIntensities, &intensity, sizeof(bool));
+Ldr.setlidaropt(LidarPropReversion, &dyreverse, sizeof(bool));
+Ldr.setlidaropt(LidarPropMaxAngle, &maxum, sizeof(float));
+Ldr.setlidaropt(LidarPropMinAngle, &minum, sizeof(float));
+Ldr.setlidaropt(LidarPropMaxRange, &scanrangemax, sizeof(float)); 
+Ldr.setlidaropt(LidarPropMinRange, &scanrangemin, sizeof(float));
+  
+return Ldr.initialize();  
+}
+
+bool start() {
+req = Ldr.inititalize(); 
+ 
+ if(req) {
+Ldr.turnOn();
+
+return true; 
+ }
+
+else {
+
+return false;
+}
+
+}
+
+void stop() {
+if(!isScanning) {
+
+return;
+}
+
+Ldr.turnOff(); 
+}
+
+void End() {
+if(!isScanning) {
+
+return;
+}
+
+Ldr.turnOff();
+Ldr.disconnecting;
+}
+
+
+
+
+
   
 
 
-}
+};
