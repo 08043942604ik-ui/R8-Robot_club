9ee3eac10e141841aa66d1845d3ad81649901ab6
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-08-10 14:48:47 +0900

    up
    Initialize Lidar node and add distance processing functionality.

--- dd/Lidar.cpp
+++ dd/Lidar.cpp
@@ -2,31 +2,37 @@
 
 
 Lidarnode::Lidarnode() : Node("lidar") {
-
-
-
-
+Lidars::initi();
+Lidars::start();
+  
+timer_ = create_wall_timer(
+  std::chrono::milliseconds(20),
+      [this]() { timer_callback(); });
   
 }
 
+double Lidarnode::disGetter() {
+if (laser_.doProcessSimple(raw_data_)) {
+        
+distanceByWalls = raw_data_.ranges;
 
+}
 
+double SendDBW(double k) {
 
+return k;
 
-
-
-
-
-
-
-
+} 
 
 
 
 void timer_callback() {
     if (!laser_.isScanning()) return;
 
-    // 最新のデータを受け取る（初期設定で開始角度を正面にセットしてある状態
-    if (laser_.doProcessSimple(raw_data_)) {
-        
-        std::vector<float> front_scan = raw_data_.ranges;
+if(dud) {
+disGetter();
+
+SendDBW(distanceByWalls);
+
+    }
+}
