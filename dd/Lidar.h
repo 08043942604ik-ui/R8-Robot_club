3b44ba234e9bcb3c6e31bc999df494fa608f168b
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-08-10 14:04:30 +0900

    up

---
+++ dd/Lidar.h
@@ -0,0 +1,25 @@
+#include "lidartest.h"
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+
+class Lidarnode : public rclcpp::Node {
+
+Lidarnode();
+
+
+void timer_callback();
+double disGetter();
+double SendDBW(double k);
+
+
+
+
+
+
+private :
+rclcpp::TimerBase::SharedPtr timer_;
+double distanceByWalls;
+
+
+
+}
