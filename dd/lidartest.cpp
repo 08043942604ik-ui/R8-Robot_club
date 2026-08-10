db4e708cecdd8108f4dcd83e6706f0e9ab411d6d
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-08-10 12:54:51 +0900

    s

--- dd/lidartest.cpp
+++ dd/lidartest.cpp
@@ -6,7 +6,8 @@
 
 
 bool Lidarinit::initi() {
-Ldr.setlidaropt(LidarPropSerialPort, port_name.c_str(), port_name.size());
+Ldr.setlidaropt(LidarPropSerialPort, port.c_str(), port.size());
+Ldr.setlidaropt(LidarPropIgnoreArray, Ignore_array.c_str(), Ignore_array.size());
 Ldr.setlidaropt(LidarPropBaudrate, &baudrate, sizeof(int));
 Ldr.setlidaropt(LidarPropLidarType, &lidar_type, sizeof(int));
 Ldr.setlidaropt(LidarPropSampleRate, &sample_rate, sizeof(int));
@@ -17,8 +18,48 @@ Ldr.setlidaropt(LidarPropScanFrequency, &scan_frequency, sizeof(float));
 Ldr.setlidaropt(LidarPropAutoReconnect, &auto_reconnect, sizeof(bool));
 Ldr.setlidaropt(LidarPropSingleChannel, &single_channel, sizeof(bool));
 Ldr.setlidaropt(LidarPropIntensities, &intensity, sizeof(bool));
-Ldr.setlidaropt(LidarPropReversion, &reversion, sizeof(bool));
- 
-
+Ldr.setlidaropt(LidarPropReversion, &dyreverse, sizeof(bool));
+Ldr.setlidaropt(LidarPropMaxAngle, &maxum, sizeof(float));
+Ldr.setlidaropt(LidarPropMinAngle, &minum, sizeof(float));
+Ldr.setlidaropt(LidarPropMaxRange, &scanrangemax, sizeof(float)); 
+Ldr.setlidaropt(LidarPropMinRange, &scanrangemin, sizeof(float));
+  
 return Ldr.initialize();  
 }
+
+bool Lidarinit::start() {
+req = Ldr.inititalize(); 
+ 
+ if(req) {
+Ldr.turnOn();
+
+return true; 
+ }
+
+else {
+
+return false;
+}
+
+}
+
+void Lidarinit::stop() {
+if(!isScanning) {
+
+return;
+}
+
+ 
+}
+
+void Lidarinit::End() {
+if(!isScanning) {
+
+return;
+}
+
+Ldr.turnOff();
+Ldr.disconnecting;
+}
+
+
