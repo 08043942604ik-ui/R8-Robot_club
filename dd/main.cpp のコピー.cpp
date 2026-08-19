be2a6c4aa965718eac3118dd49e6e304f68bf813
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-08-19 16:33:39 +0900

    update

--- dd/main.cpp のコピー.cpp
+++ dd/main.cpp のコピー.cpp
@@ -1,29 +1,35 @@
 #include "Robotmainmove.h"
 #include <stdexcept>
-
-
+#include <iostream>
+#include <General.h>
 
 int main(int argc, char * argv[]) {
     
 try {    
     rclcpp::init(argc, argv);
     auto node = std::make_shared<Robotmain>();
+    auto ganel = std::make_shared<Generalva>(); 
 
     rclcpp::executors::MultiThreadedExecutor executor;
     executor.add_node(node);
-    executor.spin(); 
-
-if(turn_thread.joinable()) {
-    turn_thread.join();
-    }
+    std::cout << "test" << std::endl;
+     std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    executor.spin();
+    std::cout << "test" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+   ganel->LWaitingThread();
     rclcpp::shutdown();
     return 0;
 }
  catch(const std::exception& e) {
 
-rclcpp::init(argc, argv);
-RCLCPP_ERROR(rclcpp::get_logger("main"), "エラーが発生しました %s", e.what());
+std::cout << "エラーが発生しました。" << std::endl;
+
+
+if (rclcpp::ok()) {
+    rclcpp::shutdown();
+}//イニシャライズしていないとROS2の終了処理に繋げられないから
 return 1;
-}  
+} 
 
 }
