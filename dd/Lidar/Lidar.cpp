01b5474f1fd1b2bb10ea0e40b3f6c1afcc1ebd99
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-08-19 16:29:01 +0900

    update

--- dd/Lidar/Lidar.cpp
+++ dd/Lidar/Lidar.cpp
@@ -1,10 +1,13 @@
 #include "Lidar.h"
+#include "lidartest.h"
 
 
-Lidarnode::Lidarnode() : Node("lidar") {
-    RCLCPP_INFO(get_logger(), "Lidar constructor");
+Lidarnode::Lidarnode()
+    : Node("lidar"), lidars_(std::make_unique<Lidarinit>())//Rdrivの中がnullptrになるから
+{
+ RCLCPP_INFO(get_logger(), "Lidar constructor");
 
-    const bool init_ok = lidars.initi();
+    const bool init_ok = lidars_->initi();
 
     RCLCPP_INFO(
         get_logger(),
@@ -15,45 +18,101 @@ Lidarnode::Lidarnode() : Node("lidar") {
         return;
     }
 
-    const bool start_ok = lidars.start();
-
-    RCLCPP_INFO(
-        get_logger(),
-        "Lidar start = %s",
-        start_ok ? "OK" : "FAILED");
 }
 
-~Lidarnode() {
-lidars.End();
+Lidarnode::~Lidarnode()
+{   
+    RCLCPP_INFO(get_logger(), "Lidar destructor called");
+    lidars_->End();
+    RCLCPP_INFO(get_logger(), "Lidar End finished");
+
+    
+}
+
+void Lidarnode::stopLidar()
+{
+    lidars_->End();
+}
+
+double Lidarnode::getDistanceAtDegree(double target_deg)
+{
+   if (raw_data_.points.empty()) {
+        return -1.0;
+    }
+
+    const double target_rad = target_deg * M_PI / 180.0;//目的の角度をラジアンに変換
+
+    const LaserPoint* nearest = &raw_data_.points[0];//0番目の点が一番近いと仮定
+
+    for (const LaserPoint& point : raw_data_.points) {
+        if (std::abs(point.angle - target_rad) <
+            std::abs(nearest->angle - target_rad)) {
+            nearest = &point;//一番近いpointのアドレスを保管
+        }
+    }
+
+    return nearest->range;
+}
+
+std::vector<float> Lidarnode::disGetter()
+{
+   if (lidars_->getScan(raw_data_)) {
+        distanceByWalls.clear();
+
+        for (const auto& point : raw_data_.points) {
+            distanceByWalls.push_back(point.range);
+        }
+    }
+
+    return distanceByWalls;
+}
+
+
+std::vector<float> Lidarnode::SendDBW(
+    const std::vector<float>& k)
+{
+    return k;
+}
+
+
+
+
+
+void Lidarnode::timer_callback()
+{
+  if(!rclcpp::ok()) return;
+
+  if(once) {
+  lidars_->start();
   
-}
+  once = false;
+  }
 
-double Lidarnode::disGetter() {
-if (laser_.doProcessSimple(raw_data_)) {
+    if (!lidars_->isScanning()) {
         
-distanceByWalls = raw_data_.ranges;
-
-}
-
-double SendDBW(double k) {
-
-return k;
-
-} 
-
-
-
-void timer_callback() {
-    if (!laser_.isScanning()) return;
-
+        return;
+    }
 if(dud) {
-lidars::initi();
-lidars::start 
-disGetter();
+  
+    const auto distances = disGetter();
 
-SendDBW(distanceByWalls);
+  RCLCPP_INFO_THROTTLE(
+            get_logger(),
+            *get_clock(),
+            1000,
+            "Lidar points=%zu first=%.3f m",
+            distanceByWalls.size(),
+            distanceByWalls.empty() ? -1.0f : distanceByWalls[0]);
+      double d = getDistanceAtDegree(0.0);
 
+
+
+  RCLCPP_INFO(get_logger(), "running dd, 0°の壁との距離%lf", d);
+    
+  SendDBW(distances);
     }
 
 
 }
+
+
