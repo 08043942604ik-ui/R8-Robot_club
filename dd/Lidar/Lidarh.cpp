5caeb909bfef563d4e9e552215c12f306f31182f
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-08-19 16:27:52 +0900

    update

--- dd/Lidar/Lidarh.cpp
+++ dd/Lidar/Lidarh.cpp
@@ -1,22 +1,29 @@
-#include "lidartest.h"
+#pragma once
+
+
 #include <rclcpp/rclcpp.hpp>
 #include <vector>
+#include <memory>
+#include "core/common/ydlidar_datatype.h" //CYdlidar.h側でVMXPi.hとぶつかるからLaserScanを使うためにこれがあるヘッダを直接インクルードする
+
+class Lidarinit;
 
 class Lidarnode : public rclcpp::Node {
+public:
+    Lidarnode();
+    ~Lidarnode();
 
-Lidarnode();
-~Lidarnode();
+    void timer_callback();
+    std::vector<float> disGetter();
+    std::vector<float> SendDBW(const std::vector<float>& k);
+    void stopLidar();
+    double getDistanceAtDegree(double target_deg);
 
-void timer_callback();
-double disGetter();
-double SendDBW(double k);
-
-
-
-private :
-rclcpp::TimerBase::SharedPtr timer_;
-std::vector<float> distanceByWalls;
-Lidarinit lidars;
-
-
-}
+private:
+    rclcpp::TimerBase::SharedPtr timer_; 
+    std::vector<float> distanceByWalls;
+    std::unique_ptr<Lidarinit> lidars_;
+    LaserScan raw_data_;
+    bool dud = true;
+    bool once = true;
+};
