2854749ad497cbc776f029ee3ecda5486e0c5552
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-09-22 10:36:10 +0900

    update

--- dd/robot_main_node/EncoderHeader.cpp
+++ dd/robot_main_node/EncoderHeader.cpp
@@ -12,7 +12,7 @@ class Lidarnode;
 
 class Encoderma : public rclcpp::Node {
  public:
-  Encoderma();
+  Encoderma(std::shared_ptr<VMXPi> vmx1);
   ~Encoderma() override;
 
   
@@ -26,15 +26,13 @@ void setspeedstop();
 void RightTurn();
 void LeftTurn();
   void StopAll();
-  void stopAll();
-
+  
   // 20ms邵ｺ譁絶・邵ｺ・ｫ陷ｻ・ｼ邵ｺ・ｰ郢ｧ蠕個繝ｻ0驕倩ｲ橸ｽｾ蠕後・陋帶㊧・ｭ・｢邵ｺ・ｨ陋滂ｽ､邵ｺ・ｮ髯ｦ・ｨ驕会ｽｺ郢ｧ螳夲ｽ｡蠕娯鴬
   void callback();
 bool right = false;
 bool left = false;
 
  private:
-  std::shared_ptr<VMXPi> vmx_;
   std::unique_ptr<studica_driver::Titan> titan_;
   rclcpp::TimerBase::SharedPtr timer_;
    std::shared_ptr<Generalva> general_;
@@ -43,4 +41,5 @@ std::array<double, 2> previous_distance_{0.0, 0.0};
   double right_delta = 0.0;
   double TotalDistance = 0.0;
   double left_delta = 0.0;
+  std::shared_ptr<VMXPi> vmx_;
 };
