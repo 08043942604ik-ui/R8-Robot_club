da738c3d78c995a04f81fba20f6a1a9ded0f28b3
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-09-22 10:33:58 +0900

    update
    Updated member variables and initialization values for RobotDrive class.

--- dd/robot_main_node/robotdriveHeader.cpp
+++ dd/robot_main_node/robotdriveHeader.cpp
@@ -23,18 +23,21 @@ class RobotDrive : public rclcpp::Node {
   bool RobotMovement(double xe, double ye);
   double turnDistanceChecker();
   void callback();
-    bool mos = false; //lidar起動フラグ
+ 
+   bool mos = false; //lidar起動フラグ
  
   private:
+
   double RobotTurn(int o);
   
   Encoderma& encoders_;
   rclcpp::TimerBase::SharedPtr timer_;
   std::array<double, 2> Xof{0.0, 0.0};
   std::atomic<bool> turnrunning{false};
-  std::atomic<double> wastedist{181.0};
+  std::atomic<double> wastedist{0.0};
   std::atomic<int> times{0};
-  std::atomic<int> Kakudo1{90};
+  std::atomic<int> Kakudo1{0};
+  std::atomic<int> Kakudo2{0};
   std::shared_ptr<Generalva> genel_;
    
 
@@ -43,13 +46,16 @@ class RobotDrive : public rclcpp::Node {
   double x = 0.0;
   double y = 0.0;
   double d = 0.0;
-  double hole = 170.0;
+  double hole = 0.0;
   double xyPa = 0.0;
   double line = 0.0;
   int delivery = 0;
-  int set = 1;
-  bool begin = true;
+  int set = 0;
+  bool begin = false;
   bool xory = false;
   bool Marn = false;
   bool Mrun = false;
- };
+  double turn_start = 0.0;
+  double turn_end = 0.0;
+  double actual_turn_dist = 0.0;
+};
