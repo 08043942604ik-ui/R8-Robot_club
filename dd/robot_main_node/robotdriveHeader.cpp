ae7d47637bbec7fff3d5bb712c214f1f53f8786e
daikichi-man <ssv-340-vpg@e-net.nara.jp>
2026-09-26 15:45:59 +0900

    s

--- dd/robot_main_node/robotdriveHeader.cpp
+++ dd/robot_main_node/robotdriveHeader.cpp
@@ -23,7 +23,9 @@ class RobotDrive : public rclcpp::Node {
   bool RobotMovement(double xe, double ye);
   double turnDistanceChecker();
   void callback();
- 
+  void AllresetA();
+  void Exchanger(bool de);
+
    bool mos = false; //lidar起動フラグ
  
   private:
@@ -39,7 +41,7 @@ class RobotDrive : public rclcpp::Node {
   std::atomic<int> Kakudo1{0};
   std::atomic<int> Kakudo2{0};
   std::shared_ptr<Generalva> genel_;
-   
+  std::atomic<bool> tum{false};
 
   double Cx = 0.0;
   double Cy = 0.0;
@@ -58,4 +60,22 @@ class RobotDrive : public rclcpp::Node {
   double turn_start = 0.0;
   double turn_end = 0.0;
   double actual_turn_dist = 0.0;
+  double turn_count = 0.0;   // 回転したときの走行距離
+  bool once = true;         // IncreaseAxis邵ｺ・ｮ隴崢陋ｻ譏ｴ笆｡邵ｺ鬘鯉ｽ｡蠕娯鴬陷・ｽｦ騾・・逡曾nint Kakudo = 0;            // 霑ｴ・ｾ陜ｨ・ｨ邵ｺ・ｮ髫苓ｲ橸ｽｺ・ｦ
+  int deg = 0;               // IncreaseAxis邵ｺ・ｧ闖ｴ・ｿ邵ｺ繝ｻ・ｧ雋橸ｽｺ・ｦ
+  double hypotenuse = 0.0;   // 陷ｴ貅ｽ縺帷ｸｺ・ｾ邵ｺ・ｧ邵ｺ・ｮ髴肴辨螻ｬ
+  double DFH = 0.0;          // atan2邵ｺ・ｧ陷・ｽｺ邵ｺ蜉ｱ笳・囓雋橸ｽｺ・ｦ
+  int AbsDegree = 0;         // 陜怜ｸｶ譏剰滋豕後・陟募ｾ後・髫苓ｲ橸ｽｺ・ｦ
+  int turndeg = 0;          // 陷ｴ貅ｽ縺帷ｸｺ・ｸ陷ｷ莉｣・･邵ｺ貅假ｽ∫ｸｺ・ｮ陜玲ｫ・ｽｻ・｢髫苓ｲ橸ｽｺ・ｦ
+  int result = 0;           // 驍ｨ蜈域｣｡
+  int add = 0;              // 90陟趣ｽｦ郢ｧ蜑・ｽｽ蜍溷ｱ楢崕繝ｻ螻鍋ｸｺ・｣邵ｺ貅伉ｰ
+  bool timer = true; //郢ｧ・ｿ郢ｧ・､郢晄ｧｭ繝ｻ郢ｧ・ｹ郢ｧ・ｿ郢晢ｽｼ郢晏現・定叉ﾂ陜玲ｧｭ竊鍋ｸｺ蜷ｶ・狗ｸｺ貅假ｽ―nbool end = false; //郢ｧ・ｿ郢ｧ・､郢晄ｧｭ繝ｻ邵ｺ・ｮ驕伜争辟夂ｹｧ雋槫徐郢ｧ荵昴・郢ｧ蜑・ｽｸﾂ陜玲ｧｭ笆｡邵ｺ莉｣竊鍋ｸｺ蜷ｶ・狗ｸｺ貅假ｽ―ndouble line = 0.0;
+  int w = 0;
+  double s = 0.0;//値確認
+  double ll = 0.0;//wastedistが入るIncreaseAxis
+  double pp = 0.0;//wastedistが入るAverageChecker
+  int checker = 0; 
+  double k = 0.0;//AverageCheckerの値入れ
+  int po = 0;
+  bool RLC = false;
 };
